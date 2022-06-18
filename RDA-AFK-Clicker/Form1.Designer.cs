1810501588f6e58a4cafad46bead147ce7a8ae11
Андрей Александрович Власов <koljisto@gmail.com>
2022-06-18 21:50:05 +0300

    Добавьте файлы проекта.

---
+++ RDA-AFK-Clicker/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace RDA_AFK_Clicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label_Gold = new System.Windows.Forms.Label();
+            this.label_BindKey = new System.Windows.Forms.Label();
+            this.domainUpDown_BindKey = new System.Windows.Forms.DomainUpDown();
+            this.button_Save = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label_Status = new System.Windows.Forms.Label();
+            this.checkBox_Enable = new System.Windows.Forms.CheckBox();
+            this.label_GoldCounter = new System.Windows.Forms.Label();
+            this.button_IsWorking = new System.Windows.Forms.Button();
+            this.button_OpenScripts = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.label_GoldMax = new System.Windows.Forms.Label();
+            this.textBox_GoldMax = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label_Gold
+            // 
+            this.label_Gold.AutoSize = true;
+            this.label_Gold.Location = new System.Drawing.Point(41, 31);
+            this.label_Gold.Name = "label_Gold";
+            this.label_Gold.Size = new System.Drawing.Size(46, 13);
+            this.label_Gold.TabIndex = 0;
+            this.label_Gold.Text = "Золото:";
+            // 
+            // label_BindKey
+            // 
+            this.label_BindKey.AutoSize = true;
+            this.label_BindKey.Location = new System.Drawing.Point(41, 57);
+            this.label_BindKey.Name = "label_BindKey";
+            this.label_BindKey.Size = new System.Drawing.Size(55, 13);
+            this.label_BindKey.TabIndex = 0;
+            this.label_BindKey.Text = "Клавиша:";
+            // 
+            // domainUpDown_BindKey
+            // 
+            this.domainUpDown_BindKey.Items.Add("F1");
+            this.domainUpDown_BindKey.Items.Add("F2");
+            this.domainUpDown_BindKey.Items.Add("F3");
+            this.domainUpDown_BindKey.Items.Add("F4");
+            this.domainUpDown_BindKey.Items.Add("F5");
+            this.domainUpDown_BindKey.Items.Add("F6");
+            this.domainUpDown_BindKey.Items.Add("F7");
+            this.domainUpDown_BindKey.Items.Add("F8");
+            this.domainUpDown_BindKey.Items.Add("F10");
+            this.domainUpDown_BindKey.Location = new System.Drawing.Point(102, 55);
+            this.domainUpDown_BindKey.Name = "domainUpDown_BindKey";
+            this.domainUpDown_BindKey.Size = new System.Drawing.Size(139, 20);
+            this.domainUpDown_BindKey.TabIndex = 2;
+            this.domainUpDown_BindKey.Tag = "";
+            this.domainUpDown_BindKey.Text = "Выбери бинд";
+            // 
+            // button_Save
+            // 
+            this.button_Save.Location = new System.Drawing.Point(41, 103);
+            this.button_Save.Name = "button_Save";
+            this.button_Save.Size = new System.Drawing.Size(200, 40);
+            this.button_Save.TabIndex = 3;
+            this.button_Save.Text = "Сохранить";
+            this.button_Save.UseVisualStyleBackColor = true;
+            this.button_Save.Click += new System.EventHandler(this.button_Save_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 146);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Статус:";
+            // 
+            // label_Status
+            // 
+            this.label_Status.AutoSize = true;
+            this.label_Status.Location = new System.Drawing.Point(91, 146);
+            this.label_Status.Name = "label_Status";
+            this.label_Status.Size = new System.Drawing.Size(37, 13);
+            this.label_Status.TabIndex = 5;
+            this.label_Status.Text = "Status";
+            // 
+            // checkBox_Enable
+            // 
+            this.checkBox_Enable.AutoSize = true;
+            this.checkBox_Enable.Location = new System.Drawing.Point(44, 5);
+            this.checkBox_Enable.Name = "checkBox_Enable";
+            this.checkBox_Enable.Size = new System.Drawing.Size(75, 17);
+            this.checkBox_Enable.TabIndex = 6;
+            this.checkBox_Enable.Text = "Включить";
+            this.checkBox_Enable.UseVisualStyleBackColor = true;
+            // 
+            // label_GoldCounter
+            // 
+            this.label_GoldCounter.AutoSize = true;
+            this.label_GoldCounter.Location = new System.Drawing.Point(103, 31);
+            this.label_GoldCounter.Name = "label_GoldCounter";
+            this.label_GoldCounter.Size = new System.Drawing.Size(13, 13);
+            this.label_GoldCounter.TabIndex = 7;
+            this.label_GoldCounter.Text = "0";
+            // 
+            // button_IsWorking
+            // 
+            this.button_IsWorking.Location = new System.Drawing.Point(166, 5);
+            this.button_IsWorking.Name = "button_IsWorking";
+            this.button_IsWorking.Size = new System.Drawing.Size(75, 23);
+            this.button_IsWorking.TabIndex = 9;
+            this.button_IsWorking.Text = "Проверка";
+            this.button_IsWorking.UseVisualStyleBackColor = true;
+            this.button_IsWorking.Click += new System.EventHandler(this.button_IsWorking_Click);
+            // 
+            // button_OpenScripts
+            // 
+            this.button_OpenScripts.Location = new System.Drawing.Point(166, 31);
+            this.button_OpenScripts.Name = "button_OpenScripts";
+            this.button_OpenScripts.Size = new System.Drawing.Size(75, 23);
+            this.button_OpenScripts.TabIndex = 10;
+            this.button_OpenScripts.Text = "Скрипты";
+            this.button_OpenScripts.UseVisualStyleBackColor = true;
+            this.button_OpenScripts.Click += new System.EventHandler(this.button_OpenScripts_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.BalloonTipText = "Я работаю";
+            this.notifyIcon1.BalloonTipTitle = "И даже в свёрнутом режиме.";
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "RDA-AFK-Clicker";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // label_GoldMax
+            // 
+            this.label_GoldMax.AutoSize = true;
+            this.label_GoldMax.Location = new System.Drawing.Point(41, 84);
+            this.label_GoldMax.Name = "label_GoldMax";
+            this.label_GoldMax.Size = new System.Drawing.Size(84, 13);
+            this.label_GoldMax.TabIndex = 11;
+            this.label_GoldMax.Text = "Лимит золота: ";
+            // 
+            // textBox_GoldMax
+            // 
+            this.textBox_GoldMax.Location = new System.Drawing.Point(131, 81);
+            this.textBox_GoldMax.Name = "textBox_GoldMax";
+            this.textBox_GoldMax.Size = new System.Drawing.Size(100, 20);
+            this.textBox_GoldMax.TabIndex = 12;
+            this.textBox_GoldMax.Text = "90000";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(271, 168);
+            this.Controls.Add(this.textBox_GoldMax);
+            this.Controls.Add(this.label_GoldMax);
+            this.Controls.Add(this.button_OpenScripts);
+            this.Controls.Add(this.button_IsWorking);
+            this.Controls.Add(this.label_GoldCounter);
+            this.Controls.Add(this.checkBox_Enable);
+            this.Controls.Add(this.label_Status);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button_Save);
+            this.Controls.Add(this.domainUpDown_BindKey);
+            this.Controls.Add(this.label_BindKey);
+            this.Controls.Add(this.label_Gold);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(220, 200);
+            this.Name = "Form1";
+            this.Text = "RDA-Clicker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResizeBegin += new System.EventHandler(this.Form1_ResizeBegin);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label_Gold;
+        private System.Windows.Forms.Label label_BindKey;
+        private System.Windows.Forms.DomainUpDown domainUpDown_BindKey;
+        private System.Windows.Forms.Button button_Save;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label_Status;
+        private System.Windows.Forms.CheckBox checkBox_Enable;
+        private System.Windows.Forms.Label label_GoldCounter;
+        private System.Windows.Forms.Button button_IsWorking;
+        private System.Windows.Forms.Button button_OpenScripts;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Label label_GoldMax;
+        private System.Windows.Forms.TextBox textBox_GoldMax;
+    }
+}
---
+++ RDA-AFK-Clicker/Form1.cs
@@ -0,0 +1,299 @@
+﻿using Dota2GSI;
+using Dota2GSI.Nodes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using WindowsInput;
+using WindowsInput.Native;
+using System.Configuration;
+using System.Text;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Drawing;
+
+namespace RDA_AFK_Clicker
+{
+    public partial class Form1 : Form
+    {
+        //const int WM_CHAR = 0x71;
+        //const int WM_SYSKEYDOWN = 0x0104;
+        //const int WM_SYSKEYUP = 0x0105;
+        //const int WM_KEYDOWN = 0x0100;
+        //const int WM_KEYUP = 0x0101;
+
+        static readonly IntPtr HWND_TOPMOST = new IntPtr(-1);
+        static readonly IntPtr HWND_NOTOPMOST = new IntPtr(-2);
+        static readonly IntPtr HWND_TOP = new IntPtr(0);
+        static readonly IntPtr HWND_BOTTOM = new IntPtr(1);
+        const UInt32 SWP_NOSIZE = 0x0001;
+        const UInt32 SWP_NOMOVE = 0x0002;
+        const UInt32 TOPMOST_FLAGS = SWP_NOMOVE | SWP_NOSIZE;
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+        //[DllImport("User32.dll")]
+        //static extern int SetForegroundWindow(IntPtr hWnd);
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+
+        /*[DllImport("user32.dll")]
+        public static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr wParam, UIntPtr lParam);*/
+        [DllImport("user32.dll")]
+        private static extern int AttachThreadInput(IntPtr CurrentForegroundThread, IntPtr MakeThisThreadForegrouond, bool boolAttach);
+        [DllImport("user32.dll")]
+        private static extern int SetFocus(IntPtr hwnd);
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetWindowThreadProcessId(IntPtr hWnd, IntPtr lpdwProcessId);
+
+        //private NotifyIcon notifyIcon1 = new NotifyIcon();
+        //notifyIcon1.Icon = Properties.Resources.Main_Icon;
+        InputSimulator sim = new InputSimulator();
+        //System.Windows.Forms.Timer TickTimer = new System.Windows.Forms.Timer();
+        Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+        Regex reg = new Regex(@"dota 2 beta");
+        Regex reg_final = new Regex(@"dota 2 beta\\game\\dota\\cfg\\gamestate_integration");
+        VirtualKeyCode selectedKeyCode;
+        //AutoHotkeyEngine ahk = AutoHotkeyEngine.Instance;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        GameStateListener gsl;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            switch (config.AppSettings.Settings["bind_key"].Value)
+            {
+                case "F1":
+                    selectedKeyCode = VirtualKeyCode.F1;
+                    domainUpDown_BindKey.SelectedIndex = 0;
+                    break;
+                case "F2":
+                    selectedKeyCode = VirtualKeyCode.F2;
+                    domainUpDown_BindKey.SelectedIndex = 1;
+                    break;
+                case "F3":
+                    selectedKeyCode = VirtualKeyCode.F3;
+                    domainUpDown_BindKey.SelectedIndex = 2;
+                    break;
+                case "F4":
+                    selectedKeyCode = VirtualKeyCode.F4;
+                    domainUpDown_BindKey.SelectedIndex = 3;
+                    break;
+                case "F5":
+                    selectedKeyCode = VirtualKeyCode.F5;
+                    domainUpDown_BindKey.SelectedIndex = 4;
+                    break;
+                case "F6":
+                    selectedKeyCode = VirtualKeyCode.F6;
+                    domainUpDown_BindKey.SelectedIndex = 5;
+                    break;
+                case "F7":
+                    selectedKeyCode = VirtualKeyCode.F7;
+                    domainUpDown_BindKey.SelectedIndex = 6;
+                    break;
+                case "F8":
+                    selectedKeyCode = VirtualKeyCode.F8;
+                    domainUpDown_BindKey.SelectedIndex = 7;
+                    break;
+                case "F10":
+                    selectedKeyCode = VirtualKeyCode.F10;
+                    domainUpDown_BindKey.SelectedIndex = 8;
+                    break;
+                default:
+                    break;
+            }
+            label_Status.Text = "Ожидаю...";
+            gsl = new GameStateListener(4000);
+            gsl.NewGameState += new NewGameStateHandler(OnNewGameState);
+            if (!gsl.Start())
+            {
+                MessageBox.Show("Пробуй запустить от администратора и проверь наличие конфига в Dota 2.\nЗакрываю.");
+                Environment.Exit(0);
+            }
+        }
+
+        void OnNewGameState(GameState gs)
+        {
+            label_GoldCounter.Text = gs.Player.Gold.ToString();
+            if (checkBox_Enable.Checked)
+            {
+                IntPtr WindowToFind = FindWindow(null, "Dota 2");
+                label_Status.Text = "В работе.";
+                if (WindowToFind != IntPtr.Zero)
+                {
+                    label_Status.Text = "В работе.";
+                    //SendMessage(WindowToFind, WM_KEYDOWN, (IntPtr)WM_CHAR, (UIntPtr)0x00410001);
+                    //SendMessage(WindowToFind, WM_KEYUP, (IntPtr)WM_CHAR, (UIntPtr)0xC0410001);
+                    if (gs.Player.Gold >= 90000)
+                    {
+                        IntPtr tmp = GetForegroundWindow();
+                        IntPtr tmpThread = GetWindowThreadProcessId(tmp, IntPtr.Zero);
+                        IntPtr WindowToFindThread = GetWindowThreadProcessId(WindowToFind, IntPtr.Zero);
+                        if(tmp == WindowToFind)
+                        {
+                            sim.Keyboard.KeyUp(VirtualKeyCode.LMENU);
+                            sim.Keyboard.KeyPress(selectedKeyCode);
+                        } else
+                        {
+                            AttachThreadInput(tmpThread, WindowToFindThread, true);
+                            AttachThreadInput(WindowToFindThread, tmpThread, false);
+                            SetFocus(WindowToFind);
+                            sim.Keyboard.KeyUp(VirtualKeyCode.LMENU);
+                            sim.Keyboard.KeyPress(selectedKeyCode);
+                            SetFocus(tmp);
+                            AttachThreadInput(tmpThread, WindowToFindThread, false);
+                            AttachThreadInput(WindowToFindThread, tmpThread, true);
+                        }
+                    }
+                }
+                else
+                {
+                    label_Status.Text = "Нет окна.";
+                }
+            } else
+            {
+                label_Status.Text = "Остановлен.";
+            }
+        }
+
+        private void button_Save_Click(object sender, EventArgs e)
+        {
+            if(domainUpDown_BindKey.SelectedItem.ToString().Length == 2 && Int32.Parse(textBox_GoldMax.ToString()) > 0)
+            {
+                switch (domainUpDown_BindKey.SelectedItem.ToString())
+                {
+                    case "F1":
+                        selectedKeyCode = VirtualKeyCode.F1;
+                        config.AppSettings.Settings["bind_key"].Value = "F1";
+                        break;
+                    case "F2":
+                        selectedKeyCode = VirtualKeyCode.F2;
+                        config.AppSettings.Settings["bind_key"].Value = "F2";
+                        break;
+                    case "F3":
+                        selectedKeyCode = VirtualKeyCode.F3;
+                        config.AppSettings.Settings["bind_key"].Value = "F3";
+                        break;
+                    case "F4":
+                        selectedKeyCode = VirtualKeyCode.F4;
+                        config.AppSettings.Settings["bind_key"].Value = "F4";
+                        break;
+                    case "F5":
+                        selectedKeyCode = VirtualKeyCode.F5;
+                        config.AppSettings.Settings["bind_key"].Value = "F5";
+                        break;
+                    case "F6":
+                        selectedKeyCode = VirtualKeyCode.F6;
+                        config.AppSettings.Settings["bind_key"].Value = "F6";
+                        break;
+                    case "F7":
+                        selectedKeyCode = VirtualKeyCode.F7;
+                        config.AppSettings.Settings["bind_key"].Value = "F7";
+                        break;
+                    case "F8":
+                        selectedKeyCode = VirtualKeyCode.F8;
+                        config.AppSettings.Settings["bind_key"].Value = "F8";
+                        break;
+                    case "F10":
+                        selectedKeyCode = VirtualKeyCode.F10;
+                        config.AppSettings.Settings["bind_key"].Value = "F10";
+                        break;
+                    default:
+                        MessageBox.Show("Введён неверный бинд клавиши. Восстановлена последняя успешная");
+                        break;
+                }
+                config.AppSettings.Settings["gold_max"].Value = textBox_GoldMax.ToString();
+                config.Save(ConfigurationSaveMode.Modified);
+                ConfigurationManager.RefreshSection("appSettings");
+            } else
+            {
+                MessageBox.Show("Введены неверные параметры");
+            }
+        }
+
+        private void button_ShowMessage_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("По пути: steamapps\\common\\dota 2 beta\\game\\dota\\cfg необходимо создать папку gamestate_integration \n" +
+                "Чтобы путь получился: steamapps\\common\\dota 2 beta\\game\\dota\\cfg\\gamestate_integration\n" +
+                "В эту папку скопировать файл, данный в архиве\n");
+        }
+
+        private void button_IsWorking_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Укажите путь до папки игры Dota 2");
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    var matched = reg.Match(fbd.SelectedPath.ToString());
+                    DirectoryInfo tmpdir = new DirectoryInfo(fbd.SelectedPath.ToString()+ "\\game\\dota\\cfg\\gamestate_integration");
+                    if (!Directory.Exists(tmpdir.ToString()))
+                    {
+                        Directory.CreateDirectory(tmpdir.ToString());
+                    }
+                    if (matched.Success)
+                    {
+                        var matched_final = reg_final.Match(tmpdir.ToString());
+                        if (matched_final.Success)
+                        {
+                            File.WriteAllText(tmpdir + "\\gamestate_integration_test.cfg",
+                            "\"Dota 2 Integration Configuration\"\n" +
+                            "{\n" +
+                            "    \"uri\"           \"http://localhost:4000/\"\n" +
+                            "    \"timeout\"       \"5.0\"\n" +
+                            "    \"buffer\"        \"0.1\"\n" +
+                            "    \"throttle\"      \"0.1\"\n" +
+                            "    \"heartbeat\"     \"30.0\"\n" +
+                            "    \"data\"\n" +
+                            "    {\n" +
+                            "        \"provider\"      \"1\"\n" +
+                            "        \"map\"           \"1\"\n" +
+                            "        \"player\"        \"1\"\n" +
+                            "        \"hero\"          \"1\"\n" +
+                            "        \"abilities\"     \"1\"\n" +
+                            "        \"items\"         \"1\"\n" +
+                            "    }\n" +
+                            "}\n"
+                            );
+                            MessageBox.Show("Файл был успешно создан.");
+                        } else
+                        {
+                            MessageBox.Show("Указан неверный путь, необходимо выбрать на подобии:\n dota 2 beta\\game\\dota\\cfg\\gamestate_integration");
+                        }
+                    }
+                }
+            }
+        }
+
+        private void button_OpenScripts_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Скрипты");
+            //SetWindowPos(this.Handle, HWND_TOPMOST, 0, 0, 0, 0, TOPMOST_FLAGS);
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            ShowInTaskbar = true;
+            notifyIcon1.Visible = false;
+            WindowState = FormWindowState.Normal;
+        }
+
+        private void Form1_ResizeBegin(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                ShowInTaskbar = false;
+                notifyIcon1.Visible = true;
+                notifyIcon1.ShowBalloonTip(1000);
+            }
+        }
+    }
+}
